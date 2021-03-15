2ff6e2940d5ae47f5d3eaff30a7a7b323c4d5ea9
maynardz <maynard.zach23@gmail.com>
2021-03-15 12:16:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header';
+import Albums from './components/Albums/Albums';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <h1 className='shopTitle' style={{textAlign: 'center', marginTop: '-4em'}}>Shop Records</h1>
+      <Albums />
     </div>
   );
 }
---
+++ src/components/albumObj.js
@@ -0,0 +1,23 @@
+import JoyDivison from '../assets/joy_division.jpg';
+import DSotM from '../assets/dsoft.jpg';
+import abbeyRoad from '../assets/abbeyRoad.jpg';
+
+const albums = [
+    {
+        image_src: JoyDivison,
+        album_title: 'Joy Division',
+        description: "Designer Peter Saville's decision to go with pulsar radio waves is right up there with Martin Hannett’s spellbinding production in making this album a goth classic. Disney's Mickey Mouse shirt parody four decades later only reaffirmed its legend."
+    },
+    {
+        image_src: DSotM,
+        album_title: 'Dark Side of the Moon',
+        description: "This simple art says so much. The light going through a prism and coming out as a rainbow was meant to convey the band's stage lighting and the album's lyrics. And, as evidenced by the number of t-shirts bearing this image today, the prism has become synonymous with Floyd itself."
+    },
+    {
+        image_src: abbeyRoad,
+        album_title: 'Abbey Road',
+        description: "Does any other album cover on this list stop traffic? It's a testament to the lasting impression of this street-crossing photo that hundreds of fans re-create it every day outside Abbey Road Studios. There's even a webcam live feed of the attraction."
+    }
+]
+
+export default albums;
