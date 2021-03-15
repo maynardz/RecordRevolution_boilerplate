88c5c797cbf4abc067ac443f49be256a9ec35886
maynardz <maynard.zach23@gmail.com>
2021-03-15 12:20:43 -0400

    remove code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header';
-import Albums from './components/Albums/Albums';
-
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Header />
-      <h1 className='shopTitle' style={{textAlign: 'center', marginTop: '-4em'}}>Shop Records</h1>
-      <Albums />
+      
     </div>
   );
 }
